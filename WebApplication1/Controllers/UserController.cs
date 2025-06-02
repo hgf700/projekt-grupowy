b8fbe5e4225bf8ae522a6dbd10ecefc51ecfb258
Przemyslaw0Dyrcz <przemyslaw.dyrcz@gmail.com>
2025-06-02 18:11:30 +0200

    widoki do edycji hasla usuniecia

--- WebApplication1/Controllers/UserController.cs
+++ WebApplication1/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using WebApplication1.Areas.Identity.Data;
 
 using WebApplication1.Models.Identity;
+using WebApplication1.Models.ViewModel;
 
 namespace WebApplication1.Controllers
 {
@@ -35,6 +36,55 @@ namespace WebApplication1.Controllers
                 .ToListAsync();
 
             return View(events);
+        }
+
+        public async Task<IActionResult> Edit()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+                return NotFound();
+
+            var model = new EditUserViewModel
+            {
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            return View(model);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EditUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+                return NotFound();
+
+            // Zmieniamy dane tylko jeśli coś się faktycznie zmienia
+            if (user.Email != model.Email)
+                user.Email = model.Email;
+
+            if (user.PhoneNumber != model.PhoneNumber)
+                user.PhoneNumber = model.PhoneNumber;
+
+            var result = await _userManager.UpdateAsync(user);
+
+            if (result.Succeeded)
+            {
+                TempData["StatusMessage"] = "Dane zostały zaktualizowane.";
+                return RedirectToAction("Details");
+            }
+
+            foreach (var error in result.Errors)
+                ModelState.AddModelError(string.Empty, error.Description);
+
+            return View(model);
         }
     }
 }
---
+++ WebApplication1/Models/ViewModel/EditUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models.Identity
+{
+    public class EditUserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        [Display(Name = "Numer telefonu")]
+        public string PhoneNumber { get; set; }
+    }
+}
